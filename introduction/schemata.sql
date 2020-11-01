c8a28cbfc722dfc0456f10b3aa359cb631fc7581
AlfinTech Computer <73285448+alfintechcomputer@users.noreply.github.com>
2020-11-01 12:02:33 +0700

    Add files via upload

---
+++ introduction/schemata.sql
@@ -0,0 +1,10 @@
++----------------------------+--------------+------+-----+---------+-------+
+| Field                      | Type         | Null | Key | Default | Extra |
++----------------------------+--------------+------+-----+---------+-------+
+| CATALOG_NAME               | varchar(512) | NO   |     |         |       |
+| SCHEMA_NAME                | varchar(64)  | NO   |     |         |       |
+| DEFAULT_CHARACTER_SET_NAME | varchar(32)  | NO   |     |         |       |
+| DEFAULT_COLLATION_NAME     | varchar(32)  | NO   |     |         |       |
+| SQL_PATH                   | varchar(512) | YES  |     | NULL    |       |
++----------------------------+--------------+------+-----+---------+-------+
+5 rows in set (0.01 sec)
---
+++ introduction/show_databases.sql
@@ -0,0 +1,16 @@
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| authdemo           |
+| blog               |
+| laratut            |
+| laravelblog        |
+| mysql              |
+| performance_schema |
+| repotut            |
+| test               |
+| wordpress          |
++--------------------+
+10 rows in set (0.00 sec)
